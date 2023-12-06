20c07d84e9fc58ad4f54e6c8e80147d10ed4075b
SofiiaTrokhymchuk <s.s.trokhymchuk@gmail.com>
2023-12-06 22:57:33 +0200

    refactored project structure

--- hw-8/src/employees/employees.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { EmployeesService } from './employees.service';
-
-@Controller('employees')
-export class EmployeesController {
-  constructor(private readonly employeesService: EmployeesService) {}
-}
